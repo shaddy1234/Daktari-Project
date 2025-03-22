9aed3d810eb9a93c66a62fe07e14cca5fa961541
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-03-22 23:35:34 +0300

    Import all files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import SymptomChecker from './pages/SymptomChecker';
+import NutritionPlanner from './pages/NutritionPlanner';
+import MentalHealth from './pages/MentalHealth';
+import Chatbot from './pages/Chatbot';
+import MedicationGuide from './pages/MedicationGuide';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <>
-     <h1 className='underline bg-blue-500 text-4xl'>Tailwind is working</h1>
-    </>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/symptom-checker" element={<SymptomChecker />} />
+          <Route path="/nutrition-planner" element={<NutritionPlanner />} />
+          <Route path="/mental-health" element={<MentalHealth />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/medication-guide" element={<MedicationGuide />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
