afe96edf6aaf2a8e67647e344e04ce04a54a0620
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-03-22 23:24:44 +0300

    Configuration of Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-     <h1>Tailwind is working</h1>
+     <h1 className='underline bg-blue-500 text-4xl'>Tailwind is working</h1>
     </>
   )
 }
